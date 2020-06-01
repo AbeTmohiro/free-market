da5278c039711b2ac04735cd9d071d457a863832
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-06-01 21:01:30 +0900

    Comment out
    確認用にコメントアウトを追加

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -1,3 +1,4 @@
+//復習用にコメントアウトを残しておきます。 
 document.addEventListener('turbolinks:load', function () {
   //新規画像投稿用のfile_fieldを作成しappendする。
   function newFileField(index) {
@@ -9,16 +10,20 @@ document.addEventListener('turbolinks:load', function () {
   if (!$('#item_form')[0]) return false;
   //商品出品・編集ページではないなら以降実行しない。
   $("#select-image-button").on("click", function () {
-    const file_field = $(".new-item-image:last"); // 新規画像投稿用のfile_fieldのを取得する。
-    file_field.trigger("click"); // file_fieldをクリックさせる。
+    // 新規画像投稿用のfile_fieldのを取得する。
+    const file_field = $(".new-item-image:last"); 
+    // file_fieldをクリックさせる。
+    file_field.trigger("click"); 
   });
-  $("#image-file-fields").on("change", `input[type="file"]`, function (e) { //新しく画像が選択された、もしくは変更しようとしたが何も選択しなかった時
+  //新しく画像が選択された、もしくは変更しようとしたが何も選択しなかった時
+  $("#image-file-fields").on("change", `input[type="file"]`, function (e) { 
     console.table(e.target.files);
     console.log("画像が選択されました")
     const file = e.target.files[0];
     let index = $(this).data("index");
     console.log("選択した画像のindex=", index);
-    const blob_url = window.URL.createObjectURL(file); //選択された画像をblob url形式に変換する。
+    //選択された画像をblob url形式に変換する。
+    const blob_url = window.URL.createObjectURL(file); 
     console.log(blob_url);
     const preview_html = `<img src="${blob_url}" width="20%">`;
     $("#select-image-button").before(preview_html);
