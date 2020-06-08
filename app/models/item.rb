1bbddf115c07a3d22eecd554f29482afa208e916
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-06-08 19:51:46 +0900

    Add scope
    オーダーと表示数を調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   validates :name, :price, :detail, :condition, :delivery_fee_payer, :delivery_method, :delivery_days, :deal, presence: true
   validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :images, length: { minimum: 1, maximum: 5, message: "の数が不正です" }
+  
+  scope :new_items, -> { order("created_at DESC").limit(4) }
 
   def self.search_by_categories(categories)
     return Item.where(category: categories).includes(:images)
