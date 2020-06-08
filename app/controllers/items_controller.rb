4269af8e0099eb1ba5eee8bee6806db66a95aa36
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-06-08 15:36:16 +0900

    Add index action in items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   
+  def index
+
+  end
+
   def new
     @item = Item.new
     @item.images.build
