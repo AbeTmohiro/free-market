db34173ba22b0552ca5d70a2f9e8a90ef3e7fea9
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2020-06-17 16:58:54 +0900

    add linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,9 @@ lock '3.12.1'
 # Capistranoのログの表示に利用する
 set :application, 'free-market'
 
+# master keyの参照
+set :linked_files, %w{ config/master.key } 
+
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:AbeTmohiro/free-market.git'
 
